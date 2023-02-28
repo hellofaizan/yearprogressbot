e0117cd17104ca37def72ef63062514d99b13c47
Mohammad Faizan <84437051+hellofaizan@users.noreply.github.com>
2023-02-28 18:37:21 +0530

    set activity

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const { Client, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permissions, MessageManager, Embed, Collection } = require(`discord.js`);
 const fs = require('fs');
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] }); 
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
 client.commands = new Collection();
 
@@ -10,12 +10,20 @@ const functions = fs.readdirSync("./src/functions").filter(file => file.endsWith
 const eventFiles = fs.readdirSync("./src/events").filter(file => file.endsWith(".js"));
 const commandFolders = fs.readdirSync("./src/commands");
 
-(async () => {
-    for (file of functions) {
-        require(`./functions/${file}`)(client);
-    }
-    client.handleEvents(eventFiles, "./src/events");
-    client.handleCommands(commandFolders, "./src/commands");
-    client.login(process.env.token)
-})();
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setActivity("with your feelings", {
+        type: "PLAYING"
+    });
+});
+
+    (async () => {
+        for (file of functions) {
+            require(`./functions/${file}`)(client);
+        }
+        client.handleEvents(eventFiles, "./src/events");
+        client.handleCommands(commandFolders, "./src/commands");
+        client.login(process.env.token)
+    })();
 
