5d78b381e78b7823d4450eca291ebaac2418e313
Mohammad Faizan <84437051+hellofaizan@users.noreply.github.com>
2023-02-28 23:06:25 +0530

    status

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permissions, MessageManager, Embed, Collection } = require(`discord.js`);
+const { Client, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permissions, MessageManager, Embed, Collection, ActivityType } = require(`discord.js`);
 const fs = require('fs');
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
@@ -10,20 +10,36 @@ const functions = fs.readdirSync("./src/functions").filter(file => file.endsWith
 const eventFiles = fs.readdirSync("./src/events").filter(file => file.endsWith(".js"));
 const commandFolders = fs.readdirSync("./src/commands");
 
+let status = [
+    {
+        name: "📐 | /setup <#channel>",
+        type: "PLAYING"
+    },
+    {
+        name: "🌍 | yp.hellofaizan.me",
+        type: "PLAYING"
+    },
+    {
+        name: `${client.guilds.cache.size} servers`,
+        type: "WATCHING"
+    }
+]
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setActivity("with your feelings", {
-        type: "PLAYING"
-    });
+    setInterval(() => {
+        let random = Math.floor(Math.random() * status.length);
+        client.user.setActivity(status[random]);
+    }, 10000);
+    console.log(`Ready to serve ${client.guilds.cache.size} servers!`);
 });
 
-    (async () => {
-        for (file of functions) {
-            require(`./functions/${file}`)(client);
-        }
-        client.handleEvents(eventFiles, "./src/events");
-        client.handleCommands(commandFolders, "./src/commands");
-        client.login(process.env.token)
-    })();
+(async () => {
+    for (file of functions) {
+        require(`./functions/${file}`)(client);
+    }
+    client.handleEvents(eventFiles, "./src/events");
+    client.handleCommands(commandFolders, "./src/commands");
+    client.login(process.env.token)
+})();
 
