d7336f74472917b7db9568991d19c5e26141a1be
Mohammad Faizan <84437051+hellofaizan@users.noreply.github.com>
2023-03-01 20:22:04 +0530

    guilds feature

---
+++ src/commands/admin/guilds.js
@@ -0,0 +1,31 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageActionRow, MessageButton } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('guilds')
+        .setDescription('Total number of servers bot active in'),
+    async execute(interaction, client) {
+        // Get total number of guilds
+        const guilds = client.guilds.cache.size;
+        // Get icon of the bot
+        const botIcon = client.user.avatarURL();
+
+        // Make an embed
+        const exampleEmbed = {
+            color: 0x0099ff,
+            title: 'Guilds',
+            description: `Currently active in ${guilds} servers!`,
+            url: 'http://yp.hellofaizan.me/',
+            timestamp: new Date(),
+            footer: {
+                name: 'Made by HelloFaizan#2746',
+                icon_url: botIcon,
+            },
+        };
+
+
+        // send the embed
+        await interaction.reply(`Currently active in ${guilds} servers!`);
+    },
+}
